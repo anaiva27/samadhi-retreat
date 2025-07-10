2885d039b90b1bc10209d2560f63861026d5881d
anaiva27 <nastia.iv.91@gmail.com>
2025-07-10 11:01:38 -0700

    predeploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-  },
+	plugins: [react()],
+	build: {
+		outDir: "build",
+		base: "/samadhi-retreat",
+	},
 });

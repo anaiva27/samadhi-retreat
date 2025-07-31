9c8db17ccff44ed7fb18a67c3a60687a71242a10
anaiva27 <nastia.iv.91@gmail.com>
2025-07-31 12:38:09 -0700

    contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -37,33 +37,6 @@ const ContactForm = () => {
 					</div>
 				</div>
 
-				{/* <div className="contact-form-col">
-					<div className="form-item">
-						<input
-							type="text"
-							placeholder="Name"
-						/>
-					</div>
-
-					<div className="form-item">
-						<input
-							type="text"
-							placeholder="Email"
-						/>
-					</div>
-
-					<div className="form-item">
-						<textarea
-							type="text"
-							rows={6}
-							placeholder="Message"
-						/>
-					</div>
-
-					<div className="form-item">
-						<button className="btn">Send Message</button>
-					</div>
-				</div> */}
 				{/* <div id="mc_embed_shell"> */}
 				<div className="contact-form-col">
 					{/* <div id="mc_embed_signup"> */}
@@ -75,8 +48,6 @@ const ContactForm = () => {
 						className="validate"
 						target="_blank"
 					>
-						{/* <div id="mc_embed_signup_scroll"> */}
-						{/* <div className="mc-field-group"> */}
 						<div className="contact-form-fields">
 							<div className="form-item">
 								<input
@@ -90,8 +61,6 @@ const ContactForm = () => {
 									onChange={(v) => setFormEmail(v.target.value)}
 								/>
 							</div>
-							{/* </div> */}
-							{/* <div className="mc-field-group"> */}
 							<div className="form-item">
 								<input
 									type="text"
@@ -103,63 +72,55 @@ const ContactForm = () => {
 									onChange={(v) => setFormName(v.target.value)}
 								/>
 							</div>
-							{/* </div> */}
-							{/* <div className="mc-field-group"> */}
 							<div className="form-item">
-								<textarea
+								<input
 									type="text"
-									name="MMERGE6"
-									className=" text"
-									id="mce-MMERGE6"
-									rows={6}
+									name="MMERGE5"
+									className="text"
+									id="mce-MMERGE5"
 									placeholder="Message"
 									value={formMessage}
 									onChange={(v) => setFormMessage(v.target.value)}
 								/>
 							</div>
-							{/* </div> */}
-							{/* <div
-									id="mce-responses"
-									className="clear"
-								>
-									<div
-										className="response"
-										id="mce-error-response"
-										style={{ display: "none" }}
-									></div>
-									<div
-										className="response"
-										id="mce-success-response"
-										style={{ display: "none" }}
-									></div>
-								</div>
+							<div
+								id="mce-responses"
+								className="clear"
+							>
 								<div
-									aria-hidden="true"
-									style={{ position: "absolute", left: "-5000px" }}
-								>
-									<input
-										type="text"
-										name="b_81d7f5090e271a5e5249e16c7_a72c961373"
-										tabindex="-1"
-										value=""
-									/>
-								</div> */}
-							{/* <div className="clear"> */}
+									className="response"
+									id="mce-error-response"
+									style={{ display: "none" }}
+								></div>
+								<div
+									className="response"
+									id="mce-success-response"
+									style={{ display: "none" }}
+								></div>
+							</div>
+							<div
+								aria-hidden="true"
+								style={{ position: "absolute", left: "-5000px" }}
+							>
+								<input
+									type="text"
+									name="b_81d7f5090e271a5e5249e16c7_a72c961373"
+									tabindex="-1"
+									value=""
+								/>
+							</div>
 							<div className="form-item">
 								<input
 									type="submit"
 									name="subscribe"
 									id="mc-embedded-subscribe"
 									className="btn"
-									value="Subscribe"
+									value="Send Message"
 									disabled={!formEmail || !formName || !formMessage}
 								/>
 							</div>
-							{/* </div> */}
-							{/* </div> */}
 						</div>
 					</form>
-					{/* </div> */}
 				</div>
 			</div>
 		</div>
